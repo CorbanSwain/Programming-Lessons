8bc410f0daa5b96844b4ea1b3e8fc582d07778e9
Corban Swain <CorbanSwain@UNDERSTANDING.local>
2017-06-02 15:27:21 -0500

    Addnl Chapter 4

--- Python/AutomoteTheBoring/Chapter3/zeroDivide.py
+++ Python/AutomoteTheBoring/Chapter3/zeroDivide.py
@@ -1,11 +1,15 @@
 def spam(divideBy):
+    print('Running spam(' + str(divideBy) + ')...')
     try:
         return 42 / divideBy
     except ZeroDivisionError:
         print('Error: Invalid argument, division by zero.')
+    except Exception as e:
+        print('Unexpected Error: ' + str(e))
 
 print(spam(2))
 print(spam(12))
-print(spam(0))
+print(spam(0) if (spam(0) != None) else '')
+print(spam('lol') if (spam('lol') != None) else '')
 print(spam(1))
       
